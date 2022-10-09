40ec8fb2fda97bd238cc9f29e9545bcc91d9d38f
Olai <Olaisenvb@gmail.com>
2022-10-09 15:40:05 +0200

    Unreleased file. Commin soon

--- Fix Windows 11.ps1
+++ Fix Windows 11.ps1
@@ -501,7 +501,7 @@ $Tab3.Controls.Add($button3)
 
 
 #========================================================
-#    Info Label test
+#    Info Label
 #========================================================
 
 $label = New-Object System.Windows.Forms.Label
